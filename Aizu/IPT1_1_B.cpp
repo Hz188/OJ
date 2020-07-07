99167808f768c6f569d300daa6381607ddacc550
haozhe <ghz@bupt.edu.cn>
2020-07-07 15:01:28 +0800

    LinkedList 双向循环链表 ALDS1_3_C

--- Aizu/IPT1_1_B.cpp
+++ Aizu/IPT1_1_B.cpp
@@ -1,10 +1,11 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main() {
+int main()
+{
 
     int x;
 
     cin >> x;
-    cout << x*x*x <<endl;
+    cout << x * x * x << endl;
     return 0;
 }
